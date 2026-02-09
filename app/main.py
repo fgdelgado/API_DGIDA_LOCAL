456fdbef2a86369b08bbafe3177a0abef30f29c6
Fernando Delgado <fernando337delgado@gmail.com>
2026-02-08 22:42:36 -0600

    se agrega magnum

--- app/main.py
+++ app/main.py
@@ -4,6 +4,7 @@ from routers.instituciones import router as instituciones_router
 from routers.tramites import router as tramites_router
 from routers.proyectos import router as proyectos_router
 from routers.programas import router as programas_router
+from mangum import Mangum
 
 app = FastAPI()
 
@@ -21,3 +22,4 @@ app.include_router(instituciones_router)
 app.include_router(tramites_router)
 app.include_router(proyectos_router)
 app.include_router(programas_router)
+handler = Mangum(app)
